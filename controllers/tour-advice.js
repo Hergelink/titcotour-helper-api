0f930404e57454be70f2f8b56ae26604c364fd0c
Enver Can Oude Hergelink <enver.hergelink@gmail.com>
2023-05-26 16:38:38 +0300

    changed route

---
+++ controllers/tour-advice.js
@@ -0,0 +1,41 @@
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const tourAdvice = async (req, res) => {
+  // input prompt coming from client
+  const userPrompt = req.body.text;
+
+  try {
+    // Title
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: userPrompt }],
+    });
+
+    const aiOutput = response.data.choices[0].message.content;
+
+    res.status(200).json({
+      success: true,
+      data: aiOutput,
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+
+    res.status(400).json({
+      success: false,
+      error: 'Prompt cannot be generated',
+    });
+  }
+};
+
+module.exports = { tourAdvice };
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const { translateGenerator } = require('../controllers/translate');
 const { translateToTurkish } = require('../controllers/translate-to-turkish');
 const { grammerChecker } = require('../controllers/grammer-check');
+const { tourAdvice } = require('../controllers/tour-advice');
 
 const router = express.Router();
 
 router.post('/translate', translateGenerator);
 router.post('/translate-to-turkish', translateToTurkish);
 router.post('/grammer-check', grammerChecker);
+router.post('/tour-advisor', tourAdvice);
 
 
 module.exports = router;
