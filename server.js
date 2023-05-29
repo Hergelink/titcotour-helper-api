8446a644e6e961a80f80b4e6f81a725846b9dd11
Enver Can Oude Hergelink <enver.hergelink@gmail.com>
2023-05-29 11:02:56 +0300

    changed server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(cors({
